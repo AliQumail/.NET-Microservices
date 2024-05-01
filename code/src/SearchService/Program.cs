be795dc569662c5558d4532a1c1431fdc53a3225
Ali Qumail <aliqumail8@gmail.com>
2024-05-01 21:32:09 +0500

    added RabbitMQ in services

--- code/src/SearchService/Program.cs
+++ code/src/SearchService/Program.cs
@@ -1,5 +1,7 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
+using System.Net;
+using MassTransit;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,12 +9,28 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+builder.Services.AddMassTransit( x => 
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+    
+    x.UsingRabbitMq( (context, cfg) => { 
+        cfg.ReceiveEndpoint("search-auction-created", e => 
+        {
+            e.UseMessageRetry(r => r.Interval(5, 5));
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 app.UseAuthorization();
 
 app.MapControllers();
@@ -30,3 +48,9 @@ app.Lifetime.ApplicationStarted.Register(async () =>
 });
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
