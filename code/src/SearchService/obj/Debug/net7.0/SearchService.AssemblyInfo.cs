da563c92610db93e12b00aee99ce342196493084
Ali Qumail <aliqumail8@gmail.com>
2024-05-05 16:23:12 +0500

    Updating auth service

--- code/src/SearchService/obj/Debug/net7.0/SearchService.AssemblyInfo.cs
+++ code/src/SearchService/obj/Debug/net7.0/SearchService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be795dc569662c5558d4532a1c1431fdc53a3225")]
 [assembly: System.Reflection.AssemblyProductAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
